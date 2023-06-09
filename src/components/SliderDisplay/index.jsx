cd1fa0ea4fc29ba8ff65365b7b57e1f78573d16d
Max Kuh <maxes2805582@gmail.com>
2023-06-09 18:58:03 +0300

    Add img

---
+++ src/components/SliderDisplay/index.jsx
@@ -0,0 +1,17 @@
+import  { Component } from 'react'
+ 
+
+class SliderDisplay extends Component {
+  render() {
+    return (
+      <>
+      <div className='WrapperSlider'>
+        <img src="./../component/img/STScI-01EVSQQZQDXK4V2BYGG8TNW9A1.png" alt="Pictures-1" />
+        HELLLLOOO
+      </div>
+      </>
+    )
+  }
+}
+
+export default SliderDisplay;
