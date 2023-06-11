30293a1f7e614beba5ff56f05b2bb1a79d041114
Max Kuh <maxes2805582@gmail.com>
2023-06-11 17:10:21 +0300

    Add autoplay,and try fixed key img

--- src/components/SliderDisplay/index.jsx
+++ src/components/SliderDisplay/index.jsx
@@ -1,9 +1,8 @@
 import { Component } from 'react';
 import styles from './styles.module.scss';
-import BtnChanger from '../NextImg';
 import imageArray from '../component/img/imgArray';
 import NewImgItem from '../ImgItem';
-const length = imageArray.length
+const length = imageArray.length;
 class SliderDisplay extends Component {
   constructor(props) {
     super(props);
@@ -11,26 +10,54 @@ class SliderDisplay extends Component {
       imgArray: structuredClone(imageArray),
       currentImg: 0,
     };
+    this.intervalSlider = null;
   }
   pastImg = () => {
     this.setState({
-      currentImg: this.state.currentImg === 0 ? 0 : this.state.currentImg - 1,
+      currentImg: this.state.currentImg === 0 ? length : this.state.currentImg - 1,
     });
   };
   nextImg = () => {
     this.setState({
       currentImg:
-        this.state.currentImg === length -1
-          ? this.state.currentImg 
+        this.state.currentImg === length - 1
+          ? this.state.currentImg
           : this.state.currentImg + 1,
     });
   };
+  stopAuto = () => {
+    clearInterval(this.intervalSlider);
+    this.intervalSlider = null;
+  };
+  startAuto = () => {
+    if (this.intervalSlider) {
+      return ;
+    }
+    this.intervalSlider = setInterval(() => {
+      this.setState({
+        currentImg: this.state.currentImg === length - 1? 0: this.state.currentImg + 1,
+      });
+    }, 300);
+    
+  };
+
   render() {
     const { imgArray, currentImg } = this.state;
     return (
       <section className={styles.wrapperSlider}>
-        <BtnChanger onClick={this.nextImg} />
-        <NewImgItem imageArray={imgArray} currentImg = {currentImg} key={currentImg}/>
+        <button onClick={this.pastImg} className={styles.btnPast}>
+          {'<'}
+        </button>
+        <button onClick={this.nextImg} className={styles.btnNext}>
+          {'>'}
+        </button>
+        <NewImgItem imageArray={imgArray} currentImg={currentImg} />
+        <button className={styles.btnAutoSlide} onClick={this.startAuto}>
+          {'∞'}
+        </button>
+        <button className={styles.btnStopSlide} onClick={this.stopAuto}>
+          {'⊠'}
+        </button>
       </section>
     );
   }
