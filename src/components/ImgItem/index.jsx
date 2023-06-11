5dcb8250688aa974e4d39b1ad65cbdd648669b84
Max Kuh <maxes2805582@gmail.com>
2023-06-11 17:11:19 +0300

    + for past commit

--- src/components/ImgItem/index.jsx
+++ src/components/ImgItem/index.jsx
@@ -1,23 +1,18 @@
-import BtnChanger from '../NextImg';
-import styles from '../SliderDisplay/styles.module.scss';
-function NewImgItem({ imageArray ,currentImg}) {
 
+import styles from '../SliderDisplay/styles.module.scss';
+function NewImgItem({ imageArray, currentImg}) {
   const slideArr = imageArray.map((itemImg, index) => {
     if (currentImg === index) {
       return (
-        <div key={itemImg.id} className={styles.imgWrapper}>
-            
-          <img
-            className={styles.img}
-            src={itemImg.src}
-            alt={itemImg.alt}
-          />
+        <div key={imageArray.id} className={styles.imgWrapper}>
+          <img className={styles.img} src={itemImg.src} alt={itemImg.alt} />
           <p className={styles.textUnderImg}>{itemImg.alt}</p>
         </div>
       );
     }
+    return null;
   });
-  return <>{slideArr}</>;
+  return <>{slideArr }</>;
 }
 
 export default NewImgItem;
