9cebedf31bfbf4e9a32b5fb836bd899253f21170
Max Kuh <maxes2805582@gmail.com>
2023-06-11 17:35:22 +0300

    Fixed img key props

--- src/components/SliderDisplay/index.jsx
+++ src/components/SliderDisplay/index.jsx
@@ -1,27 +1,30 @@
 import { Component } from 'react';
 import styles from './styles.module.scss';
-import imageArray from '../component/img/imgArray';
+import { imageData, lengthImgArr } from '../component/img/imgArray';
 import NewImgItem from '../ImgItem';
-const length = imageArray.length;
+
 class SliderDisplay extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgArray: structuredClone(imageArray),
+      imgArray: structuredClone(imageData),
       currentImg: 0,
     };
     this.intervalSlider = null;
   }
   pastImg = () => {
     this.setState({
-      currentImg: this.state.currentImg === 0 ? length : this.state.currentImg - 1,
+      currentImg:
+        this.state.currentImg === 0
+          ? lengthImgArr - 1
+          : this.state.currentImg - 1,
     });
   };
   nextImg = () => {
     this.setState({
       currentImg:
-        this.state.currentImg === length - 1
-          ? this.state.currentImg
+        this.state.currentImg === lengthImgArr - 1
+          ? 0
           : this.state.currentImg + 1,
     });
   };
@@ -31,14 +34,16 @@ class SliderDisplay extends Component {
   };
   startAuto = () => {
     if (this.intervalSlider) {
-      return ;
+      return;
     }
     this.intervalSlider = setInterval(() => {
       this.setState({
-        currentImg: this.state.currentImg === length - 1? 0: this.state.currentImg + 1,
+        currentImg:
+          this.state.currentImg === lengthImgArr - 1
+            ? 0
+            : this.state.currentImg + 1,
       });
-    }, 300);
-    
+    }, 3000);
   };
 
   render() {
@@ -51,7 +56,7 @@ class SliderDisplay extends Component {
         <button onClick={this.nextImg} className={styles.btnNext}>
           {'>'}
         </button>
-        <NewImgItem imageArray={imgArray} currentImg={currentImg} />
+        <NewImgItem imgArray={imgArray} currentImg={currentImg} />
         <button className={styles.btnAutoSlide} onClick={this.startAuto}>
           {'∞'}
         </button>
